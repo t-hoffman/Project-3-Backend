91e42e32b6c645eb6dfa958e7b7ca3f0edef3e2d
Tyler Hoffman <tjhoffmn@gmail.com>
2023-01-09 16:18:17 -0800

    added user route

--- models/homes.js
+++ models/homes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const homeSchema = new mongoose.Schema({
+    userId: Number,
     name: {type: String, required: [true, 'Name Required']},
     address: {type: String, require: [true, 'Address Required']},
     location: {
